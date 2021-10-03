785128a7e3aa96535430a073b1941b350a172344
imansaparov <malikimansaparov@gmail.com>
2021-10-03 08:38:05 +0600

    feat: implement tabs and data send to server

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,24 @@
 import "./slider";
 import modals from './modules/modals';
+import tabs from './modules/tabs';
+import forms from './modules/form';
 
 window.addEventListener('DOMContentLoaded', () => {
-    modals();
+  'use strict';
+  modals();
+  tabs('.glazing_slider ', '.glazing_block', '.glazing_content', 'active');
+  tabs(
+    '.decoration_slider',
+    '.no_click',
+    '.decoration_content > div > div',
+    'after_click'
+  );
+  tabs(
+    '.balcon_icons',
+    '.balcon_icons_img',
+    '.big_img > img',
+    'do_image_more',
+    'inline-block'
+  );
+  forms();
 });
--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -1,8 +1,14 @@
 const modals = () => {
-  function bindModal(triggerSelector, modalSelector, closeSelector) {
+  function bindModal(
+    triggerSelector,
+    modalSelector,
+    closeSelector,
+    closeClickOverlay = true
+  ) {
     const trigger = document.querySelectorAll(triggerSelector),
       modal = document.querySelector(modalSelector),
-      close = document.querySelector(closeSelector);
+      close = document.querySelector(closeSelector),
+      windows = document.querySelectorAll('[data-modal]');
 
     trigger.forEach((item) => {
       item.addEventListener('click', (e) => {
@@ -10,20 +16,33 @@ const modals = () => {
           e.preventDefault();
         }
 
+        windows.forEach((item) => {
+          //overlay state filling the form
+          item.style.display = 'none';
+        });
+
         modal.style.display = 'block';
         document.body.style.overflow = 'hidden';
-        // document.body.classList.add('modal-open');
+        // document.body.classList.add('modal-open');//аnalogue hidden
       });
     });
 
     close.addEventListener('click', () => {
+      windows.forEach((item) => {
+        item.style.display = 'none';
+      });
+
       modal.style.display = 'none';
       document.body.style.overflow = '';
       // document.body.classList.remove('modal-open');
     });
 
     modal.addEventListener('click', (e) => {
-      if (e.target === modal) {
+      if (e.target === modal && closeClickOverlay) {
+        windows.forEach((item) => {
+          item.style.display = 'none';
+        });
+
         modal.style.display = 'none';
         document.body.style.overflow = '';
         // document.body.classList.remove('modal-open');
@@ -44,7 +63,20 @@ const modals = () => {
     '.popup_engineer .popup_close'
   );
   bindModal('.phone_link', '.popup', '.popup .popup_close');
-  showModalByTime('.popup', 30000);
+  bindModal('.popup_calc_btn', '.popup_calc', '.popup_calc_close');
+  bindModal(
+    '.popup_calc_button',
+    '.popup_calc_profile',
+    '.popup_calc_profile_close',
+    false
+  );
+  bindModal(
+    '.popup_calc_profile_button',
+    '.popup_calc_end',
+    '.popup_calc_end_close',
+    false
+  );
+  // showModalByTime('.popup', 60000);
 };
 
 export default modals;
